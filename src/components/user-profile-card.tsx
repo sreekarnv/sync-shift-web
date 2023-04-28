5deb93886a50ee7205034b86831aef035639ea83
Sreekar <sreekarnv1@gmail.com>
2023-04-28 16:04:14 +0530

    fix svg icons

--- src/components/user-profile-card.tsx
+++ src/components/user-profile-card.tsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import FeatherEditIcon from './icons/feather-edit.icon';
 import Avatar from 'boring-avatars';
 import { User } from '@/providers/app-provider';
+import FeatherCoffeeIcon from './icons/feather-coffee.icon';
+import FeatherMailIcon from './icons/feather-mail.icon';
 
 const UserProfileCard: React.FC<{ user: User; showEdit?: boolean }> = ({
   user,
@@ -35,43 +37,12 @@ const UserProfileCard: React.FC<{ user: User; showEdit?: boolean }> = ({
             <div className="">
               <ul className="contacts-block list-unstyled">
                 <li className="contacts-block__item d-flex items-center">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    className="feather feather-coffee me-3"
-                  >
-                    <path d="M18 8h1a4 4 0 0 1 0 8h-1"></path>
-                    <path d="M2 8h16v9a4 4 0 0 1-4 4H6a4 4 0 0 1-4-4V8z"></path>
-                    <line x1="6" y1="1" x2="6" y2="4"></line>
-                    <line x1="10" y1="1" x2="10" y2="4"></line>
-                    <line x1="14" y1="1" x2="14" y2="4"></line>
-                  </svg>{' '}
-                  {user.role}
+                  <FeatherCoffeeIcon />
+                  <span className="ms-3">{user.role}</span>
                 </li>
                 <li className="contacts-block__item text-truncate">
-                  <a href={`mailto:${user.email}`}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="feather feather-mail me-3"
-                    >
-                      <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path>
-                      <polyline points="22,6 12,13 2,6"></polyline>
-                    </svg>
+                  <FeatherMailIcon />
+                  <a className="ms-3" href={`mailto:${user.email}`}>
                     {user.email}
                   </a>
                 </li>
